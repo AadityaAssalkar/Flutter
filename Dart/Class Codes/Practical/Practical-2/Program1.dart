f88ecd948a0ff8d0a4dfb378c95d40ccbc6ebaca
AadiBhai <aadityaassalkar@gmail.com>
2023-09-03 14:03:49 +0530

    Practical 2 executed

---
+++ Dart/Class Codes/Practical/Practical-2/Program1.dart
@@ -0,0 +1,8 @@
+// All the codes from this practical are for dry run and already dry run in hard copy
+// Guess the output
+
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    print("C2W");
+  }
+}
---
+++ Dart/Class Codes/Practical/Practical-2/Program5.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  int x = 1;
+  for (int i = 1; i <= 4; i++) {
+    for (int j = 1; j <= 2; j++) {
+      stdout.write("${x++} "); // here {} is necessary to use ++ operator
+    }
+    print("");
+  }
+}
